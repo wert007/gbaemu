edfc78872e6af3d95561ddf96264d4e1ef9c77a1
wert007 <pablohamacher@web.de>
2025-02-12 16:11:54 +0100

    Backup with small commits

---
+++ disasm/src/main.rs
@@ -0,0 +1,29 @@
+use unarm::{DisplayOptions, RegNames};
+
+fn main() {
+    let bios = include_bytes!("../../assets/foss_bios.bin");
+    for (addr, _inst, p) in unarm::Parser::new(
+        unarm::ParseMode::Arm,
+        0,
+        unarm::Endian::Little,
+        unarm::ParseFlags {
+            ual: true,
+            version: unarm::ArmVersion::V5Te,
+        },
+        bios,
+    ) {
+        println!(
+            "{addr:x}:\t{} (0x{:x})",
+            p.display(DisplayOptions {
+                reg_names: RegNames {
+                    av_registers: false,
+                    r9_use: unarm::R9Use::GeneralPurpose,
+                    explicit_stack_limit: false,
+                    frame_pointer: false,
+                    ip: false
+                }
+            }),
+            _inst.code(),
+        );
+    }
+}
